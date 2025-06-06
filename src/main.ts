9972506c13cedb4a1fe041def7748590161486ea
Rafael Andeloci <rafael.goncalves@bkbank.com.br>
2025-06-06 14:23:43 -0300

    feat: adding swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Exploding Gattos API V1')
+    .setDescription('The Exploding Gattos Api!!!🐈🧨')
+    .setVersion('1.0')
+    .addTag('exploding-gattos')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.API_PORT ?? 3000);
 }
 bootstrap();
